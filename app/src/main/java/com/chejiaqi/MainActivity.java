c6baa4dc7dcb8f2ca77e12ada75effccafb8865f
Rajan Maurya <rajanmaurya154@gmail.com>
2017-01-30 16:35:42 +0530

    initial commit

---
+++ app/src/main/java/com/chejiaqi/MainActivity.java
@@ -0,0 +1,34 @@
+package com.chejiaqi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fab_scheduler)
+    FloatingActionButton mFloatingActionButton;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+    }
+
+    @OnClick(R.id.fab_scheduler)
+    void onClickScheduler() {
+        Intent intent = new Intent(this, SchedulerActivity.class);
+        startActivity(intent);
+    }
+}
